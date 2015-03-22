b59e7ddbed453a97febc45d7ad95794a5a73b0ae
Carlos Rosão <shemahmforash@hotmail.com>
2015-03-22 18:19:12 +0000

    Changed user schema

--- settings.py
+++ settings.py
@@ -1,1 +1,31 @@
-DOMAIN = {'user': {}}
+DOMAIN = {
+    'user': {
+        'schema': {
+            'firstname': {
+                'type': 'string'
+            },
+            'lastname': {
+                'type': 'string'
+            },
+            'username': {
+                'type': 'string',
+                 'unique': True
+            },
+            'password': {
+                'type': 'string'
+            },
+            'phone': {
+                'type': 'string'
+            }
+        }
+    }
+}
+
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
+# individual items  (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
